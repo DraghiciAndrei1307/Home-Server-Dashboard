6020de64b5f6d8ab3df207615cc4cc5a69516b72
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-06-06 19:27:58 +0300

    init(home_server_dashboard): the first commit

---
+++ home_server_dashboard/setup.py
@@ -0,0 +1,38 @@
+"""
+    This package represents is responsible
+     for creating a dashboard web app used
+     for my home server.
+"""
+
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'This is the home_server_dashboard module.'
+LONG_DESCRIPTION = (
+    'This is a web application that contains '
+    'a dashboard web page for my home server.'
+)
+AUTHOR = 'Andrei Draghici'
+AUTHOR_EMAIL = 'draghici.andrei12@yahoo.com'
+
+setup(
+    name='home_server_dashboard_draghici_andrei',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author='Andrei Draghici',
+    author_email='draghici.andrei12@yahoo.com',
+    url='https://github.com/draghiciandrei1307/python-automation',
+    packages=find_packages(),
+    install_requires=[
+        'flask'
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'run_server='
+        ]
+    }
+
+)
